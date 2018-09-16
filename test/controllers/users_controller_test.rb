639c794de439ae066d81865f32ac23cc5ec535d0
k4ssyi <kasikasi00@gmail.com>
2018-09-16 09:14:09 +0000

    Add user microposts

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,4 +54,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+  
+    test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
 end
